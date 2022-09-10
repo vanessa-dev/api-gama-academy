4cbf39651f839d1a78f9fe7c676953fec15b5af1
Vanessa Santana <vanessa_santanatoledo@outlook.com>
2022-09-10 11:36:31 -0300

    feat: adicionando autenticação com jwt.

--- server.js
+++ server.js
@@ -1,10 +1,120 @@
+const fs = require('fs');
+const bodyParser = require('body-parser');
 const jsonServer = require('json-server');
+const jwt = require('jsonwebtoken');
+
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const router = jsonServer.router('./database.json');
+const userdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'));
 const port = process.env.PORT || 4000;
 
-server.use(middlewares);
-server.use(router);
+server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json())
+server.use(jsonServer.defaults());
+
+const SECRET_KEY = process.env.SECRET_KEY;
+const expiresIn = '1h'
+
+
+function createToken(payload){
+  return jwt.sign(payload, SECRET_KEY, {expiresIn})
+}
+
+function verifyToken(token){
+  return  jwt.verify(token, SECRET_KEY, (err, decode) => decode !== undefined ?  decode : err)
+}
+
+// Checando se o usuario existe no banco de dados.
+function isAuthenticated({email, password}){
+  return userdb.users.findIndex(user => user.email === email && user.password === password) !== -1
+}
+
+// Registrando novo usuário.
+server.post('/auth/register', (req, res) => {
+  console.log("register endpoint called; request body:");
+  console.log(req.body);
+  const {email, password} = req.body;
+
+  if(isAuthenticated({email, password}) === true) {
+    const status = 401;
+    const message = 'Email and Password already exist';
+    res.status(status).json({status, message});
+    return
+  }
+
+fs.readFile("./users.json", (err, data) => {  
+  if (err) {
+    const status = 401
+    const message = err
+    res.status(status).json({status, message})
+    return
+  };
+
+  // Pegando os dados atual.
+  var data = JSON.parse(data.toString());
+
+  // Pegando o id do ultimo usuário.
+  var last_item_id = data.users[data.users.length-1].id;
+
+  //Adicionando novo usuário
+  data.users.push({id: last_item_id + 1, email: email, password: password}); //add some data
+  var writeData = fs.writeFile("./users.json", JSON.stringify(data), (err, result) => {  // WRITE
+      if (err) {
+        const status = 401
+        const message = err
+        res.status(status).json({status, message})
+        return
+      }
+  });
+});
+
+// Criando token para novo usuário
+const access_token = createToken({email, password})
+  console.log("Token de acesso:" + access_token);
+  res.status(200).json({access_token})
+})
+
+// Logando com um dos usuarios em  ./users.json
+server.post('/auth/login', (req, res) => {
+  console.log("login endpoint called; request body:");
+  console.log(req.body);
+  const {email, password} = req.body;
+  if (isAuthenticated({email, password}) === false) {
+    const status = 401
+    const message = 'Email ou senha inválido.'
+    res.status(status).json({status, message})
+    return
+  }
+  const access_token = createToken({email, password})
+  console.log("Access Token:" + access_token);
+  res.status(200).json({access_token})
+})
+
+server.use(/^(?!\/auth).*$/,  (req, res, next) => {
+  if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
+    const status = 401
+    const message = 'Erro no formato do token de  autorização'
+    res.status(status).json({status, message})
+    return
+  }
+  try {
+    let verifyTokenResult;
+     verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
+
+     if (verifyTokenResult instanceof Error) {
+       const status = 401
+       const message = 'Token de acesso não fornecido'
+       res.status(status).json({status, message})
+       return
+     }
+     next()
+  } catch (err) {
+    const status = 401
+    const message = 'Token expirado'
+    res.status(status).json({status, message})
+  }
+})
+
+server.use(router)
 
 server.listen(port);
