0fc2bfa8114a2cd47eabecccc06778fffb2b15f2
Vanessa Santana <vanessa_santanatoledo@outlook.com>
2022-09-14 23:46:25 -0300

    refactor: rotas de  transacoes

--- routes/server.js
+++ routes/server.js
@@ -4,6 +4,7 @@ const jsonServer = require('json-server');
 
 const {createUser, login, verifyAuth} = require("./auth/index.js");
 const {createCategory, listCategory, updateCategory, deleteCategory} = require("./categoria/index.js");
+const {createTransaction, listTransaction, updateTransaction, deleteTransaction} = require("./transacao/index.js");
 
 const server = jsonServer.create();
 const port = process.env.PORT || 4000;
@@ -24,6 +25,12 @@ server.get('/categoria/:id', (req, res) => listCategory(req, res));
 server.put('/categoria/:id', (req, res) => updateCategory(req, res));
 server.delete('/categoria/:id', (req, res) => deleteCategory(req, res));
 
+// Rotas de Transacoes
+server.post('/transacao', (req, res) => createTransaction(req, res));
+server.get('/transacao', (req, res) => listTransaction(req, res));
+server.get('/transacao/:id', (req, res) => listTransaction(req, res));
+server.put('/transacao/:id', (req, res) => updateTransaction(req, res));
+server.delete('/transacao/:id', (req, res) => deleteTransaction(req, res));
 
 
 server.listen(port);
---
+++ routes/transacao/index.js
@@ -0,0 +1,85 @@
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const { transacao } = JSON.parse(fs.readFileSync('db.json'), {encoding:'utf8'});
+
+
+function createTransaction(req, res) {
+  fs.readFile("./db.json", (err, data) => {  
+    if (err) {
+      const status = 401
+      const message = err
+      res.status(status).json({status, message})
+      return
+    };
+
+    var data = JSON.parse(data.toString());
+    const { tipo, valor, categoria, descricao, data } = req.body;
+    const add_data =  {
+      id: uuidv4(),  
+      tipo, 
+      valor, 
+      categoria, 
+      descricao, 
+      data
+    };
+    data.transacao.push(add_data);
+    fs.writeFile("./db.json", JSON.stringify(data), (err, result) => {  
+      if (err) {
+        const status = 401
+        const message = err
+        res.status(status).json({status, message})
+        return
+      }
+
+      res.status(201).json(add_data);
+    });
+  });
+}
+
+function listTransaction (req, res) {
+  const empty_object_params = Object.entries(req.params).length === 0;
+  const empty_object_query = Object.entries(req.query).length === 0;
+  if (!empty_object_params) {
+    const transaction_filter =   transacao.filter( item => item.id == req.params.id);
+    res.status(200).json(transaction_filter);
+  } else if (!empty_object_query) {
+    const transaction_filter_query =   transacao.filter(item => item.descricao == req.query.descricao);
+    res.status(200).json(transaction_filter_query);
+  } else {
+    res.status(200).json(categoria);
+  }
+}
+
+function updateTransaction (req, res) {
+
+}
+
+function deleteTransaction(req, res) {
+  const empty_object_params = Object.entries(req.params).length === 0;
+  if (!empty_object_params) {
+    fs.readFile("./db.json", (err, data) => {  
+      if (err) {
+        const status = 401
+        const message = err
+        res.status(status).json({status, message})
+        return
+      };
+      
+      var data = JSON.parse(data.toString());
+      const delete_transaction =  data.categoria.findIndex(item => item.id == req.params.id );
+      data.categoria.splice(delete_transaction, 1);
+      fs.writeFile("./db.json", JSON.stringify(data), (err, result) => {  
+        if (err) {
+          const status = 401
+          const message = err
+          res.status(status).json({status, message})
+          return
+        }
+        res.status(200).json({});
+      });
+    });
+    
+  }
+}
+
+module.exports = {createTransaction, listTransaction, updateTransaction, deleteTransaction}
