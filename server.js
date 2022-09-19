83de4117d0c05689e8b3c46fc0667eed651416ee
Vanessa Santana <vanessa_santanatoledo@outlook.com>
2022-09-19 08:17:36 -0300

    feat: retornando o middleware

--- server.js
+++ server.js
@@ -7,29 +7,29 @@ const {buscarTransacao, gerarUID, LancamentoComCategoria} = require("./middlewar
 server.use(middlewares);
 
 //middleware
-// server.use(jsonServer.bodyParser);
-// server.use((req, res, next) => {
-//   if (req.method === 'GET' && req.originalUrl.includes("/transacao")) {
-//     res.status(200).json(buscarTransacao());
-//     return;
-//   }
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === 'GET' && req.originalUrl.includes("/transacao")) {
+    res.status(200).json(buscarTransacao());
+    return;
+  }
 
-//   if (req.method === 'POST') {
-//     req.body.id = gerarUID();
-//     next();
-//     return;
-//   }
+  if (req.method === 'POST') {
+    req.body.id = gerarUID();
+    next();
+    return;
+  }
 
-//   if (req.method === 'DELETE' && req.originalUrl.includes("/categoria")) {
-//     if(LancamentoComCategoria) {
-//       res.status(403).json({message: "Não e possivel excluir essa categoria, pois existe transação associada a ela"});
-//       return;
-//     }
-//     next();
-//   }
-//   // Continue to JSON Server router
-//   next();
-// })
+  if (req.method === 'DELETE' && req.originalUrl.includes("/categoria")) {
+    if(LancamentoComCategoria) {
+      res.status(403).json({message: "Não e possivel excluir essa categoria, pois existe transação associada a ela"});
+      return;
+    }
+    next();
+  }
+  // Continue to JSON Server router
+  next();
+});
 
 server.use(router);
 
